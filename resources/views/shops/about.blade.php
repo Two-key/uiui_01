8ca4b86d3d5fd61f4f7d0135386378ae89d7ceff
Two-key <21ue006@mb2.fwu.ac.jp>
2023-10-15 13:48:27 +0900

    検索機能実装

--- resources/views/shops/about.blade.php
+++ resources/views/shops/about.blade.php
@@ -6,7 +6,7 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 </head>
 <body>
-<h1>{{ $town->t_name }}の情報</h1>
+<h1>{{ $town->name }}の情報</h1>
 <div class='towns'>
     <form action="/{{$town->id}}/towns" method="POST">
         @csrf
@@ -14,12 +14,12 @@
     </form>
 </div>
 <h2 class='title'>最新情報</h2>
-<a href="/towninformation/{{ $town->id }}">view</p>
+<a href="/towninformation/{{ $town->id }}">view</a>
 <h2 class='title'>まちのお悩み掲示板</h2>
-<a href="/trouble/{{ $town->id }}">view</p>
+<a href="/trouble/{{ $town->id }}">view</a>
 <h2 class='title'>基本ルール</h2>
-<a href="/rule/{{ $town->id }}">view</p>
+<a href="/rule/{{ $town->id }}">view</a>
 <h2 class='title'>お店</h2>
-<a href="/search/{{ $town->id }}">view</p>
+<a href="/search/{{ $town->id }}">view</a>
 </body>
 </x-app-layout>
