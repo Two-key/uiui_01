ff50e22531aeb23ddf42988b78e7e862d34958a9
Two-key <21ue006@mb2.fwu.ac.jp>
2023-11-09 12:20:23 +0900

    welcome画面からlogin画面へのルート変更

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ShopController;
 use App\Http\Controllers\SearchController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
